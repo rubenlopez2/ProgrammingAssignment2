fe635d3c01c764089c1dd2ab3c5bafeaa1c057fd
Ruben Lopez <ruben.lopez2@gmail.com>
2014-05-22 23:54:48 -0500

    sets up makeCacheMatrix function & commentary
    Sets up the functions to manipulate the matrix. The steps taken by the
    function are commented.

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix makes a list of functions that work with the
+## Matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Set the value of the inverse matrix to null.
+  
+  inv <- NULL
+  
+  ## Creates the function to set a matrix. the values are saved to
+  ## the parent frame.
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## Function to get matrix.
+  
+  get <- function() x
+  
+  ## Sets the value for the solved inverse of the matrix.
+  
+  setinv <- function(inverse) inv <- inverse
+  
+  ## Gets the value of the inverse.
+  
+  getinv <- function() inv
+  
+  ## Creates the list of functions.
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function tests to see if there is a previously calculated 
+## inverse value has been calculated previously. If it has not, it
+## will calculate one.
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   
+  ## Return a matrix that is the inverse of 'x'
   ## calls the function to get the value of the inverse
   
   inv <- x$getinv()
-  ## Tests to see if there is a previously calculated inverse value.
+  
   ## If inv is not null, then the condition will be true and will 
   ## display a message indicating that it is using cached data
   ## and then display the data and exit the function.
@@ -28,9 +53,11 @@ cacheSolve <- function(x, ...) {
   ## Pulls the matrix to perform the calculations.
   
   data <- x$get()
+  
   ## Calculates the inverse of the square matrix.
   
   inv <- solve(data,...)
+  
   ## Calls the function to set the value of the inverse of the matrix.
   
   x$setinv(inv)
